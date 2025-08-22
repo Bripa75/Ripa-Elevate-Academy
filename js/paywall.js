31b91cbd61266192785202eea77b28cd3ef3ce60
Bripa75 <ripabrian93@gmail.com>
2025-08-21 21:24:46 -0400

    Create paywall.js

---
+++ js/paywall.js
@@ -0,0 +1,32 @@
+<script>
+// Simple paywall helper for Ripa Elevate Academy
+(function () {
+  function getParam(name) {
+    const m = new RegExp('[?&]' + name + '=([^&]*)').exec(location.search);
+    return m ? decodeURIComponent(m[1].replace(/\+/g, ' ')) : null;
+  }
+
+  // If Stripe redirected with ?access=diag, set local unlock
+  const access = getParam('access');
+  if (access === 'diag') {
+    try {
+      localStorage.setItem('rea_diag_paid', '1');
+    } catch (e) {}
+  }
+
+  // Public function: call this at top of locked pages
+  window.requireDiagAccess = function () {
+    try {
+      const ok = localStorage.getItem('rea_diag_paid') === '1';
+      if (!ok) location.replace('lock.html');
+    } catch (e) {
+      location.replace('lock.html');
+    }
+  };
+
+  // Optional helper to clear access (for testing)
+  window.clearDiagAccess = function () {
+    try { localStorage.removeItem('rea_diag_paid'); } catch (e) {}
+  };
+})();
+</script>
